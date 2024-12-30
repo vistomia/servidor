ada1ad5314e2a979ba021826d7d13fc07e7b7edc
vctorfarias <victor.farias.0108@gmail.com>
2024-12-30 19:57:47 -0300

    doc: link do app

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/servidor/'
-    : '/',
-  transpileDependencies: true
-})
+    : '/'
+}
